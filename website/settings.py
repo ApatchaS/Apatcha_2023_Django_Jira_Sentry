344694718b418f3cc7ecfe0d6727ff9acf1c1d50
Apatcha <s.apatchenko@yandex.ru>
2023-01-27 20:22:00 +0300

    fix: rename class environment_checker

--- website/settings.py
+++ website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from .website_utils.environment_checker import Environment
+from .website_utils.environment import Environment
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
