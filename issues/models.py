e47d78113d2d586988a2a8612c99dc7dfb226301
Apatcha <s.apatchenko@yandex.ru>
2023-02-15 23:03:57 +0300

    fix: slight changes in migrations and models.py

--- issues/models.py
+++ issues/models.py
@@ -5,7 +5,7 @@ class JiraAuth(models.Model):
 	"""
 Jira's bearer auth tokens
 	"""
-	token = models.CharField(max_length=100, unique=True) #Unique
+	token = models.CharField(max_length=100, unique=True)
 
 	def __str__(self):
 		return f'Jira\'s token: {self.token}'
@@ -14,21 +14,21 @@ class JiraConnection(models.Model):
 	"""
 Jira's session connect configuration
 	"""
-	base_url = models.URLField(unique=True) #Unique
-	timeout = models.IntegerField(default=30) #Not required
+	base_url = models.URLField(unique=True)
+	timeout = models.PositiveSmallIntegerField(default=30)
 
 	def __str__(self):
-		return f'Base url {self.base_url} with connection timeout {self.timeout}'
+		return f'Base url: "{self.base_url}" with connection timeout: {self.timeout}'
 
 class Jira(models.Model):
 	"""
 Jira's projects
 	"""
-	project_name = models.CharField(max_length=50, unique=True) #Unique
-	project_id = models.IntegerField(unique=True) #Unique
-	auth = models.OneToOneField(JiraAuth, on_delete=models.RESTRICT) #One to one relation
-	connection = models.OneToOneField(JiraConnection, on_delete=models.RESTRICT) #One to one relation
-	last_updated = models.DateTimeField(default=timezone.now) #Not required
+	project_name = models.CharField(max_length=50, unique=True)
+	project_id = models.IntegerField(unique=True)
+	auth = models.OneToOneField(JiraAuth, on_delete=models.RESTRICT)
+	connection = models.OneToOneField(JiraConnection, on_delete=models.RESTRICT)
+	last_updated = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
 		return f'{self.project_name}:{self.project_id}'
@@ -37,8 +37,8 @@ class Sentry(models.Model):
 	"""
 Sentry's projects
 	"""
-	project_name = models.CharField(max_length=50, unique=True) #Unique
-	last_updated = models.DateTimeField(default=timezone.now) #Not required
+	project_name = models.CharField(max_length=50, unique=True)
+	last_updated = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
 		return self.project_name
@@ -47,13 +47,13 @@ class Issue(models.Model):
 	"""
 Issues from Sentry to post as tasks in Jira
 	"""
-	sentry_project_name = models.ForeignKey(Sentry, on_delete=models.CASCADE) #One to many relation
+	sentry_project_name = models.ForeignKey(Sentry, on_delete=models.CASCADE)
 	type = models.CharField(max_length=100)
 	value = models.CharField(max_length=100)
 	traceback = models.JSONField()
 	url = models.URLField()
-	date = models.DateTimeField(default=timezone.now) #Not required
-	sent = models.BooleanField(default=False) #Not required
+	date = models.DateTimeField(default=timezone.now)
+	sent = models.BooleanField(default=False)
 
 	def __str__(self):
 		return f'{self.sentry_project_name}: {self.type} "{self.value}"'
@@ -62,8 +62,8 @@ class JiraSentryLink(models.Model):
 	"""
 Link between projects in Jira and Sentry
 	"""
-	jira_project_name = models.OneToOneField(Jira, on_delete=models.CASCADE) #Unique
-	sentry_project_name = models.OneToOneField(Sentry, on_delete=models.CASCADE) #Unique
+	jira_project_name = models.OneToOneField(Jira, on_delete=models.CASCADE)
+	sentry_project_name = models.OneToOneField(Sentry, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return f'{self.jira_project_name}:{self.sentry_project_name}'
