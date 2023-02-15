c11efc132c303f60894d9ec249a393ac07321a22
Apatcha <s.apatchenko@yandex.ru>
2023-02-15 23:01:18 +0300

    feat: functionality of command delete_outdated_sentry_projects were increased - possible to delete Jira projects

--- issues/management/commands/delete_outdated_projects.py
+++ issues/management/commands/delete_outdated_projects.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
-from issues.models import Sentry
+from issues.models import Sentry, Jira
 import datetime
 import logging
 
@@ -8,9 +8,13 @@ DEFAULT_TIME_IN_DAYS=30
 logger = logging.getLogger('site')
 
 class Command(BaseCommand):
-	help = 'Clears the database of Sentry projects that have <days> no new issue'
+	help = 'Clears the database of projects that have <days> no new update'
 
 	def add_arguments(self, parser):
+		parser.add_argument('model',
+							help='Model name from which is needed to delete projects',
+							choices=['Sentry', 'Jira'],
+		      				)
 		parser.add_argument('days',
 		      				nargs='?',
 		      				type=int,
@@ -23,29 +27,31 @@ class Command(BaseCommand):
 							help='Disable warning before deletion',)
 		
 	def handle(self, *args, **options):
-		logger.debug('Command-line command delete_outdated_sentry_projects were called')
+		model_name = options['model']
+		logger.debug(f'Command-line command delete_outdated_projects for {model_name} model were called')
+		model = Sentry if model_name=='Sentry' else Jira
 		current_time = timezone.now()
-		objects_to_delete = Sentry.objects.exclude(
+		objects_to_delete = model.objects.exclude(
 				last_updated__gte=current_time - datetime.timedelta(days=options['days']),
 				last_updated__lte=current_time
 		)
 		number_of_projects_to_delete = len(objects_to_delete)
 		if not options['force']:
-			warning = input(f'Are you sure to delete {number_of_projects_to_delete} projects? (Y/N): ')
+			warning = input(f'Are you sure to delete {number_of_projects_to_delete} projects from {model_name} model? (Y/N): ')
 			if warning != 'Y':
-				output_message = 'The deletion of sentry projects has been canceled'
+				output_message = f'The deletion of {model_name} projects has been canceled'
 				logger.info(output_message)
 				return self.style.HTTP_NOT_MODIFIED(output_message)
 		try:
 			result = objects_to_delete.delete()
-			output_message = f'Successfuly deleted {result[0]} projects from sentry datatable'
+			output_message = f'Successfuly deleted {result[0]} projects from {model_name} datatable'
 			logger.info(output_message)
 			return self.style.SUCCESS(output_message)
 		except:
-			output_message = f'Unable to delete {number_of_projects_to_delete}; \
+			output_message = f'Unable to delete {number_of_projects_to_delete} projects from {model_name} model; \
 		 					command arguments: \
-		 					*records-{len(Sentry.objects.all())} \
-		 					*time_in_days-{options["days"]} \
-		 					*force_flag-{options["force"]}'
+		 					*records-{len(model.objects.all())} \
+		 					*time in days-{options["days"]} \
+		 					*force flag-{options["force"]}'
 			logger.error(output_message)
 			raise CommandError(output_message)
